7276d75cbb43d797e64f06b2ed94c6f784eb332f
V4L3NT1N-B <valentin.berbett@laposte.net>
2022-12-21 10:17:10 +0100

    Add BDD

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,56 +17,49 @@ export class LoginComponent implements OnInit {
   ident: Array<Array<string>> = []
 
   ngOnInit(): void {
-    this.router.navigate(['login']);
-    if (localStorage.getItem('checked?') == "false" || localStorage.getItem("checked?") == null){
-      localStorage.clear();
-    } else {
-      var mail1 = document.getElementById("mail") as HTMLInputElement | null;
-      var mdp1 = document.getElementById("mdp") as HTMLInputElement | null;
-      $("#mail").val() == localStorage.getItem('mail');
-      if (mail1 != null && mdp1 != null){
-        mail1.value = String(localStorage.getItem('mail'));
-        mdp1.value = String(localStorage.getItem('mdp'));
-      }
-    }
-    //en arrivant sur la page de login le set user + mdp est reset pour pas bypass la page de login
-    var ch = this.router;
-    //var ident = this.ident;
-    //on peut envoyer des messages en meme temps qu'un redirect info interessante
+	this.router.navigate(['login']);
+	if (localStorage.getItem('checked?') == "false" || localStorage.getItem("checked?") == null){
+	  localStorage.clear();
+	} else {
+	  var mail1 = document.getElementById("mail") as HTMLInputElement | null;
+	  var mdp1 = document.getElementById("mdp") as HTMLInputElement | null;
+	  $("#mail").val() == localStorage.getItem('mail');
+	  if (mail1 != null && mdp1 != null){
+		mail1.value = String(localStorage.getItem('mail'));
+		mdp1.value = String(localStorage.getItem('mdp'));
+	  }
+	}
+	//en arrivant sur la page de login le set user + mdp est reset pour pas bypass la page de login
+	var ch = this.router;
+	//var ident = this.ident;
+	//on peut envoyer des messages en meme temps qu'un redirect info interessante
 
-    (async () => {
-      var liste = await getData();
-      $("#submit").on('click', function(e){
-        e.preventDefault();
-        var mail = document.getElementById("mail") as HTMLInputElement | null;
-        var mdp =  document.getElementById("mdp") as HTMLInputElement | null;
-        var check=$("#check").is(":checked");
+	(async () => {
+		var liste = await getData();
+		$("#submit").on('click', function(e){
+			e.preventDefault();
+			var mail = document.getElementById("mail") as HTMLInputElement | null;
+			var mdp =  document.getElementById("mdp") as HTMLInputElement | null;
+			var check=$("#check").is(":checked");
 
-        if ($("#mail").val() == 1 && $("#mdp").val() == 1){
-          console.log("oui");
-          localStorage.setItem('mail',String($("#mail").val()));
-          localStorage.setItem('mdp',String($("#mdp").val()));
-          localStorage.setItem('checked?', String(check));
-          ch.navigate(['home']);
-        }
-        if (mail != null && mdp != null){
-          for (var i =0; i<liste.length; i++){
-            if (liste[i].login == $("#mail").val() || $("#mail").val() == "Valentin"){
-              if (liste[i].mdp == $("#mdp").val() || $("#mdp").val() == "123"){
-                //ident.push(smth);
-                localStorage.setItem('mail',String($("#mail").val()));
-                localStorage.setItem('mdp',String($("#mdp").val()));
-                localStorage.setItem('checked?', String(check));
-                //redirect
-                ch.navigate(['home']);
-                //ch.navigate(['home'], {state: {data: ident}});
-                //pour envoyer le message avec
-              }
-            }
-          }
-        }
-      })
+			if (mail != null && mdp != null){
+				for (var i =0; i<liste.length; i++){
+					if (liste[i].login == $("#mail").val() || $("#mail").val() == "Valentin"){
+						if (liste[i].mdp == $("#mdp").val() || $("#mdp").val() == "123"){
+							//ident.push(smth);
+							localStorage.setItem('mail',String($("#mail").val()));
+							localStorage.setItem('mdp',String($("#mdp").val()));
+							localStorage.setItem('checked?', String(check));
+							//redirect
+							ch.navigate(['home']);
+							//ch.navigate(['home'], {state: {data: ident}});
+							//pour envoyer le message avec
+						}
+					}
+				}
+			}
+		})
 
-    })();
+	})();
   }
 }
