18cc117de23e8798f68c1304abe820d6dd9255b7
V4L3NT1N-B <valentin.berbett@laposte.net>
2022-12-23 11:12:16 +0100

    submenu HP

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -29,28 +29,35 @@ export class HomePageComponent implements OnInit {
 
 
 		
-      var scrollState = "top";
-  
-      $(window).scroll(function() {
-        if(($(window).scrollTop() != 0 ) && (scrollState === "top")) {
-          $(".align-header").css({"background-color": "#141414", "transition": "background-color 0.3s"});
-          scrollState = "scrolled";
-        }
-        else if (($(window).scrollTop() === 0 ) && (scrollState === "scrolled")) {
-          $(".align-header").css("background-color", "transparent");
-          scrollState = "top";
-        }
-      });
+    var scrollState = "top";
+
+    $(window).scroll(function() {
+      if(($(window).scrollTop() != 0 ) && (scrollState === "top")) {
+        $(".align-header").css({"background-color": "#141414", "transition": "background-color 0.3s"});
+        scrollState = "scrolled";
+      }
+      else if (($(window).scrollTop() === 0 ) && (scrollState === "scrolled")) {
+        $(".align-header").css("background-color", "transparent");
+        scrollState = "top";
+      }
+    });
     
   
-      var firsttime = 3000;
-      var secondtime = 10000;
-  
-      $(".synopsis").delay(firsttime).slideToggle(400).delay(secondtime).slideToggle(400);
-      $(".netflix-production").delay(firsttime).animate({"height": "20px", "margin-right": "0"}).delay(secondtime).animate({"height": "35px", "margin-right": "10px"});
-      $("#title-main h2").delay(firsttime).animate({"font-size": "16px"}).delay(secondtime).animate({"font-size": "24px"});
-      $("#title-main h1").delay(firsttime).animate({"font-size": "32px", "margin-bottom": "16px"}).delay(secondtime).animate({"font-size": "64px", "margin-bottom": "0"});
-  
+    var firsttime = 3000;
+    var secondtime = 10000;
+
+    $(".synopsis").delay(firsttime).slideToggle(400).delay(secondtime).slideToggle(400);
+    $(".netflix-production").delay(firsttime).animate({"height": "20px", "margin-right": "0"}).delay(secondtime).animate({"height": "35px", "margin-right": "10px"});
+    $("#title-main h2").delay(firsttime).animate({"font-size": "16px"}).delay(secondtime).animate({"font-size": "24px"});
+    $("#title-main h1").delay(firsttime).animate({"font-size": "32px", "margin-bottom": "16px"}).delay(secondtime).animate({"font-size": "64px", "margin-bottom": "0"});
+
+    $("#account").on("mouseenter", function(){
+      $("#menu-account").fadeIn(150);
+      $("#menu-account").on("mouseleave", function(){
+        $("#menu-account").fadeOut(150);
+      });
+    });
+
   }
 
 }
